b45957aee673cf328f92d2476b03f19d71ef9dc6
Flavio Schutzer de Godoy <63651210+FlavioSG-BR@users.noreply.github.com>
2026-02-13 21:26:47 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "challenge-rg" {
+resource "azurerm_resource_group" "rg" {
   name     = "challenge-rg"
   location = "West Europe"
 }
